62738e1f3178cb8cc7a850930ef7ec0bb3feaffe
Jhordan <jhordan.sullca@tecsup.edu.pe>
2026-05-20 23:25:41 -0500

    Le agregue más codigo pue

--- app_arbol/arbol_logica.py
+++ app_arbol/arbol_logica.py
@@ -23,3 +23,35 @@ class NodoRuta:
                 return True
 
         return False
+
+    def contar_nodos(self):
+        contador = 1
+
+        for hijo in self.hijos:
+            contador += hijo.contar_nodos()
+
+        return contador
+
+    def calcular_altura(self):
+        if not self.hijos:
+            return 1
+
+        alturas = []
+
+        for hijo in self.hijos:
+            alturas.append(hijo.calcular_altura())
+
+        return 1 + max(alturas)
+
+    def obtener_hijos(self):
+        return [hijo.dato for hijo in self.hijos]
+
+    def eliminar_hijo(self, valor):
+        for hijo in self.hijos:
+            if hijo.dato.lower() == valor.lower():
+                self.hijos.remove(hijo)
+                return True
+            elif hijo.eliminar_hijo(valor):
+                return True
+        return False
+

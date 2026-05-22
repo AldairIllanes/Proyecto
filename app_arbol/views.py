98876c85bf21449685cd8a051fcdc225ac7bed59
yefersonquispe-cloud <yeferson.quispe@tecsup.edu.pe>
2026-05-21 23:26:49 -0500

    Agregué las views del proyecto

--- app_arbol/views.py
+++ app_arbol/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .arbol_logica import NodoRuta
 
-# Create your views here.
+raiz = NodoRuta("Reclamos")
+academico = NodoRuta("Académico")
+administrativo = NodoRuta("Administrativo")
+tecnico = NodoRuta("Técnico")
+
+raiz.agregar_hijo(academico)
+raiz.agregar_hijo(administrativo)
+raiz.agregar_hijo(tecnico)
+academico.agregar_hijo(NodoRuta("Matrícula"))
+academico.agregar_hijo(NodoRuta("Notas"))
+administrativo.agregar_hijo(NodoRuta("Pagos"))
+tecnico.agregar_hijo(NodoRuta("Aula Virtual"))
+
+def inicio(request):
+    return render(request, 'index.html')
+
+def ver_arbol(request):
+    contexto = {
+        'nodos': raiz.recorrer(),
+        'total': raiz.contar_nodos(),
+        'altura': raiz.calcular_altura()
+    }
+    return render(request, 'arbol.html', contexto)
+
+def buscar_reclamo(request):
+    resultado = None
+    if request.method == 'POST':
+        valor = request.POST.get('buscar')
+        if raiz.buscar(valor):
+            resultado = f"Sí existe el reclamo: {valor}"
+        else:
+            resultado = f"No existe el reclamo: {valor}"
+    return render(request, 'buscar.html', {
+        'resultado': resultado
+    })
+
+def registrar_reclamo(request):
+    return render(request, 'registrar.html')
+# DETALLE
+def detalle_reclamo(request):
+    return render(request, 'detalle.html')
